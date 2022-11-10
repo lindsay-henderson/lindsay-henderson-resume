11267e3b56dc9ae5b2c12a6c171e231485b915bb
LH <lindsaynhenderson@gmail.com>
2022-11-10 16:50:11 -0600

    start projects file inside data folder

---
+++ src/data/projects.js
@@ -0,0 +1,11 @@
+
+[
+{
+  title: '5kFox',
+  description: 'A digital pet. User helps fox train for a 5k by giving them naps, cookies, and regular runs',
+  image: ' ',
+  repositoryLink: 'https://github.com/lindsay-henderson/5k-Fox.git',
+  deploymentLink: 'https://5k-fox.netlify.app/'
+
+}
+]
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 function Contact() {
   return (
     <section>
-        <h1>Let's Chat</h1>
+        <h1>Let's Chat!</h1>
         <p>Email</p>
         <a href="https://www.linkedin.com/in/lindsay-n-henderson/"><p>LinkedIn</p></a>
         <a href="https://github.com/lindsay-henderson">GitHub</a>
