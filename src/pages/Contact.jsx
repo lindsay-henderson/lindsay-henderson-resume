28fee973409b4da895ae7bb2b0c2b32309a2f45b
LH <lindsaynhenderson@gmail.com>
2022-11-13 15:37:01 -0600

    style contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,14 @@
+import '../styles/contact.css'
+
 function Contact() {
   return (
-    <section>
+    <section className="chat">
         <h1>Let's Chat!</h1>
-        <p>Email</p>
+        <p>LindsayNHenderson@gmail.com</p>
+        <div className="contact">
         <a href="https://www.linkedin.com/in/lindsay-n-henderson/"><p>LinkedIn</p></a>
-        <a href="https://github.com/lindsay-henderson">GitHub</a>
+        <a href="https://github.com/lindsay-henderson"><p>GitHub</p></a>
+        </div>
       </section>
   )
 }
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,4 +1,4 @@
-import '../styles/Resume.css'
+import '../styles/resume.css'
 function Resume() {
   return (
   <body>
