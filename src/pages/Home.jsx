ea8797384b63edb368de0698ed2d33fdeaef2d1d
LH <lindsaynhenderson@gmail.com>
2022-11-22 16:13:43 -0600

    add image to about page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 function Home() {
   return (
     <div className="home-card">
-    <h2>Lindsay Henderson</h2>
-    <h3>Software Engineer</h3>
+    <img src="https://live.staticflickr.com/65535/52515959358_6223be8c33_b.jpg" alt="a photo of Lindsay"/>
+    <h2>Hi, My name is Lindsay Henderson</h2>
+    <h3>I am a software engineer</h3>
     </div>
   )
 }
