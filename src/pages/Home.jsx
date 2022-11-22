bcff34ac9ab09b94447a4c447e143b8c113751b8
LH <lindsaynhenderson@gmail.com>
2022-11-22 16:50:41 -0600

    format layout for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
+import'../styles/home.css'
+
 function Home() {
   return (
     <div className="home-card">
-    <img src="https://live.staticflickr.com/65535/52515959358_6223be8c33_b.jpg" alt="a photo of Lindsay"/>
-    <h2>Hi, My name is Lindsay Henderson</h2>
-    <h3>I am a software engineer</h3>
+      <div className="headshot">
+        <img src="https://live.staticflickr.com/65535/52517586536_d1e1debd7e_b.jpg" alt="a photo of Lindsay"/>
+      </div>
+      <div className='text'>
+        <h2>Hi, My name is Lindsay Henderson</h2>
+        <h4>Welcome to my website.  Please check out the links above to learn more about me.</h4>
+      </div>
     </div>
   )
 }
