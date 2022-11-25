f8da305f583a384f972eb3f0caccc6202b663a24
LH <lindsaynhenderson@gmail.com>
2022-11-25 11:49:18 -0600

    everything functional

--- src/components/ProjectPreview.jsx
+++ src/components/ProjectPreview.jsx
@@ -1,9 +1,9 @@
 import '../styles/projects.css'
-import hyphenateWords from '../utilities/hyphenateWords'
-import { Link } from 'react-router-dom'
+// import hyphenateWords from '../utilities/hyphenateWords'
+// import { Link } from 'react-router-dom'
 
 const ProjectPreview = (props) => {
-  let path = hyphenateWords(props.title)
+  // let path = hyphenateWords(props.title)
     
   return(
     <div className='preview'>
@@ -13,7 +13,7 @@ const ProjectPreview = (props) => {
       />
       <section className ='title'>
         <h3>{props.title}</h3>
-        <p>{props.description}</p>
+        <p className='projectDescription'>{props.description}</p>
         <p>{props.techs}</p>
         {/* <Link to={`/projects/${path}`}>
           <button>Learn more</button>
