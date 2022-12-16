26a192a1e2fc27e3a584a70a11b2d76d1c911868
LindsayH <lindsaynhenderson@gmail.com>
2022-12-16 13:34:51 -0600

    update nav bar anchor spacing

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,17 @@
 function Nav() {
   return (
 
-        <nav className="nav-bar">
-          <div className="home">
-            <a href='#home-page'>
-              <img src="/apple-touch-icon.png" alt="a square with the initials L & H"/>
-            </a>
-
-          <div className="links">
-          <a href='#projects'>Projects</a>
-          <a href='#about'>About</a>
-          <a href='#resume'>Resume</a>
-          </div>
-          </div>
-        </nav>
+    <nav className="nav-bar">
+      <div className="home">
+        <a href='#home-page'>
+          <img src="/apple-touch-icon.png" alt="a square with the initials L & H"/>
+        </a>
+      <a href='#projects'>Projects</a>
+      <a href='#about'>About</a>
+      <a href='#resume'>Resume</a>
+      </div>
+    </nav>
   )
 }
-          //   <NavLink to='/about'>About</NavLink>
-          //   <NavLink to={pdf} target="_blank">Resume</NavLink>
-          //   <NavLink to='/contact'>Contact</NavLink>
 
 export default Nav
