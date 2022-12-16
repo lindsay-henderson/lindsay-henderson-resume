4d833cb23e51b3ebaaaccc13015889be444ab6dd
LindsayH <lindsaynhenderson@gmail.com>
2022-12-16 12:35:48 -0600

    change color of contact icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,7 @@ import '../styles/contact.css'
 
 function Contact() {
   return (
+    <section>
     <section className='contact-box'>
       <h1>Let's Stay in Touch</h1>
       
@@ -9,15 +10,12 @@ function Contact() {
         <div className="contact">
           <p> <img src="/envelope.png" alt="mail icon" />LindsayNHenderson@gmail.com</p>
             <div className='icons'>
-            <a href="https://www.linkedin.com/in/lindsay-n-henderson/" rel="noreferrer" target="_blank"> <img src="/linkedin.png" alt="linkedin logo" /></a>
-            <a href="https://github.com/lindsay-henderson" rel="noreferrer" target="_blank"><img src="/github.png" alt="github logo" /></a>
+            <a href="https://www.linkedin.com/in/lindsay-n-henderson/" rel="noreferrer" target="_blank"> <img src="/linkedin-light.png" alt="linkedin logo" /></a>
+            <a href="https://github.com/lindsay-henderson" rel="noreferrer" target="_blank"><img src="/github-white.png" alt="github logo" /></a>
           </div>
         </div>
-        <div className='intro'>
-          <img src ="/photos/lobstercat.jpg"
-          alt="a cat wearing a lobster hat"/>
-        </div>
       </div>
+      </section>
       </section>
   )
 }
