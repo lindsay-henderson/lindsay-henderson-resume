ba26bebb26e29a7b02093824a01d14f3218f0871
LindsayH <lindsaynhenderson@gmail.com>
2022-12-16 22:11:23 -0600

    add margin around mail icon

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ function Contact() {
       
       <div className="chat">
         <div className="contact">
-          <p> <img src="/envelope-light.png" alt="mail icon" />LindsayNHenderson@gmail.com</p>
+          <p> <img src="/envelope-light.png" alt="mail icon"/>LindsayNHenderson@gmail.com</p>
             <div className='icons'>
             <a href="https://www.linkedin.com/in/lindsay-n-henderson/" rel="noreferrer" target="_blank"> <img src="/linkedin-light.png" alt="linkedin logo" /></a>
             <a href="https://github.com/lindsay-henderson" rel="noreferrer" target="_blank"><img src="/github-white.png" alt="github logo" /></a>
